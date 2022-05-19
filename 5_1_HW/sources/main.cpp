632f53ecc46b2a15f762002cab33e90073b2daeb
IvanKhrol <70334861+IvanKhrol@users.noreply.github.com>
2022-05-19 22:30:33 +0300

    add shared_ptr

---
+++ 5_1_HW/sources/main.cpp
@@ -0,0 +1,9 @@
+#include "shared_ptr.hpp"
+#include <memory>
+
+
+int main() {
+    custom_classes::shared_ptr<int> sh_ptr_1 {new int {7}};
+    std::cout << *sh_ptr_1 << std::endl;
+    return 0;
+}  
